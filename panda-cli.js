63b6ce252a61bb85f48c13312b28e72c2995f0a6
Adam Puzio <adam_puzio@cable.comcast.com>
2022-09-06 13:54:48 -0400

    Updated export object of panda-cli.js

--- panda-cli.js
+++ panda-cli.js
@@ -7,19 +7,45 @@ const Commands = {
   'project:create': require('./bin/panda-project/create'),
   'project:info': require('./bin/panda-project/info'),
   'project:start': require('./bin/panda-project/start'),
-  //scaffold
+  // route
+  'route:create': require('./bin/panda-route/create'),
+  // scaffold
+  'scaffold:create': require('./bin/panda-scaffold/create'),
   'scaffold:list': require('./bin/panda-scaffold/list'),
   // service
   'service:create': require('./bin/panda-service/create'),
   // test
   'test:logging': require('./bin/panda-test/logging'),
   // other
-  ctx: require('./bin/panda-ctx')
+  ctx: require('./bin/panda-ctx'),
+  install: require('./bin/panda-install'),
+  uninstall: require('./bin/panda-uninstall')
 }
 
-const Scaffolds = {}
+const Scaffolds = {
+  Command: {
+    basic: require('./scaffolds/command/basic/scaffold'),
+    scaffold: require('./scaffolds/command/scaffold/scaffold')
+  },
+  Project: {
+    api: require('./scaffolds/project/api/scaffold'),
+    cli: require('./scaffolds/project/cli/scaffold'),
+    web: require('./scaffolds/project/web/scaffold')
+  },
+  Route: {
+    default: require('./scaffolds/route/default/scaffold')
+  },
+  Scaffold: {
+    default: require('./scaffolds/scaffold/default/scaffold')
+  },
+  Service: {
+    basic: require('./scaffolds/service/basic/scaffold')
+  }
+}
 
 module.exports = {
   Commands,
-  Scaffolds
+  Scaffolds,
+
+  Scaffold: require('./src/entity/scaffold')
 }
